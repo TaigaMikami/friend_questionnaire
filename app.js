7c26331aeacca15c5f405b854fe4d07851e8fd33
TaigaMikami <mikamimikami60@gmail.com>
2017-10-12 06:23:21 +0900

    githubログインとテストwo

--- app.js
+++ app.js
@@ -4,11 +4,44 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var helmet = require('helmet');
+
+var session = require('express-session');
+var passport = require('passport');
+var GitHubStrategy = require('passport-github2').Strategy;
+
+var GITHUB_CLIENT_ID = '98976f31becfc3e9e1e6';
+var GITHUB_CLIENT_SECRET = '20d1e87cc29b3cac0a6d050067e11ed38a7b6762';
+
+//ユーザー情報をデータとして保存する処理
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+//保存されたデータをユーザーの情報として読み出す際の処理
+passport.deserializeUser(function (obj, done) {
+  done(null, obj);
+});
+
+passport.use(new GitHubStrategy({
+  clientID: GITHUB_CLIENT_ID,
+  clientSecret: GITHUB_CLIENT_SECRET,
+  callbackURL: 'http://localhost:8000/auth/github/callback'
+},
+  function (accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+      return done(null, profile);
+    });
+  }
+));
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var login = require('./routes/login');
+var logout = require('./routes/logout');
+
 
 var app = express();
+app.use(helmet());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +55,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({ secret: 'b421053efc766581', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', routes);
-app.use('/users', users);
+app.use('/login', login);
+app.use('/logout', logout);
+
+app.get('/auth/github',
+ passport.authenticate('github', { scope: ['user:email'] }),
+ function (req, res) {
+});
+
+app.get('/auth/github/callback',
+ passport.authenticate('github', { failureRedirect: '/login' }),
+ function (req, res) {
+  res.redirect('/');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
